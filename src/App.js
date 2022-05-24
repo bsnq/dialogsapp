5a4157021a6f41e756986a04d32ec1f8d10adebb
bsnq <75063690+bsnq@users.noreply.github.com>
2022-05-24 19:21:22 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/Header";
+import Dialog from './components/Dialog';
+
+const App = () => {
+  return (
+    <div className="container">
+      <Header />
+      <Dialog />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Header/UserInfo/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./../UserInfo/index.css";
+
+const UserInfo = () => (
+  <div className="user-info">
+    <div className="name">bsnq</div>
+    <div className="status">
+      <span className="circle" />В сети
+    </div>
+  </div>
+);
+
+export default UserInfo;
